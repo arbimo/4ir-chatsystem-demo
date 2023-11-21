26fdcff6166089590320a7b977c0dc10f57a2836
Arthur Bit-Monnot <abitmonnot@laas.fr>
2023-11-21 13:11:30 +0100

    Initial UDP server implementation

--- src/main/java/chatsystem/Main.java
+++ src/main/java/chatsystem/Main.java
@@ -1,8 +1,23 @@
 package chatsystem;
 
 
+import chatsystem.network.UDPServer;
+
+import java.net.SocketException;
+
 public class Main {
+
+    public static final int PORT = 1789;
+
     public static void main(String[] args) {
-	System.out.println("Welcome to the ChatSystem program");
+        System.out.println("Welcome to the ChatSystem program");
+
+        try {
+            UDPServer server = new UDPServer(PORT);
+            server.start();
+        } catch (SocketException e) {
+            System.err.println("Could not start UDP server: " + e.getMessage());
+            System.exit(1);
+        }
     }
 }
---
+++ src/main/java/chatsystem/network/UDPServer.java
@@ -0,0 +1,35 @@
+package chatsystem.network;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+/** Create a UDP server that (once started) listens indefinitely on a given port. */
+public class UDPServer extends Thread {
+
+    private final DatagramSocket socket;
+
+    public UDPServer(int port) throws SocketException {
+        socket = new DatagramSocket(port);
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                byte[] buff = new byte[1024];
+                DatagramPacket packet = new DatagramPacket(buff, buff.length);
+
+                // wait for the next message
+                socket.receive(packet);
+
+                // extarct and print message
+                String received = new String(packet.getData(), 0, packet.getLength());
+                System.out.println("Received: " + received);
+            } catch (IOException e) {
+                System.err.println("Receive error: " + e.getMessage());
+            }
+        }
+    }
+}
